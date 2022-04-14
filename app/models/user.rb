2ca7b5d2e2e0afea4e5787cc02d6b7e1a009ee10
Ahmed <ahmedreda152@gmail.com>
2022-04-14 08:22:45 +0200

    add validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :recipes, foreign_key: 'user_id', dependent: :destroy
   has_many :foods, foreign_key: 'user_id', dependent: :destroy
 
+
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+
   def recipes
     recipes.order('created_at DESC').limit(3)
   end
