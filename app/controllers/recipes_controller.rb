59a66b481abf14a5eb216a6a0e3dea1c3c42dc69
Ahmed <ahmedreda152@gmail.com>
2022-04-15 15:12:57 +0200

    finish index page for receipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = current_user.recipes 
+    @recipes = current_user.recipes
   end
 
   def new
@@ -8,24 +8,42 @@ class RecipesController < ApplicationController
   end
 
   def show
-   @recipe = Recipe.find(params[:id])
-   @user = @recipe.user
-   @recipe_foods = @recipe.recipe_foods
+    @recipe = Recipe.find(params[:id])
+    @user = @recipe.user
+    @recipe_foods = @recipe.recipe_foods
   end
 
   def create
-    @recipe= current_user.recipes.new(recipe_params)
+    @recipe = current_user.recipes.new(recipe_params)
     if @recipe.save
-    format.html { redirect_to recipes_path, notice: 'Recipe created successfully' }
+      format.html { redirect_to recipes_path, notice: 'Recipe created successfully' }
     else
       format.html { redirect_to new_recipe_path, notice: 'something went wrong, Please try again' }
     end
   end
-  
+
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
     flash[:notice] = 'Recipe removed successfully'
     redirect_to recipes_path
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe_state = params[:public]
+    @checked = false
+    if @recipe_state == true
+      checked = true
+    else
+      checked
+    end
+    @recipe.update_attribute(:public, checked)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
