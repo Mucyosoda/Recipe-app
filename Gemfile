3b214febaa7a1edd1390b771b9d99b545b47d4d7
Ahmed <ahmedreda152@gmail.com>
2022-04-16 11:22:35 +0200

    add devise to app and make authentication

--- Gemfile
+++ Gemfile
@@ -76,3 +76,4 @@ group :test do
   gem 'webdrivers'
 end
 gem 'rails-controller-testing'
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'recipe_foods/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "foods#index"
-  resources :foods
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'foods#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+    resources :foods
   resources :recipes do
     resources :recipe_foods, only: %i[create destroy new]
   end
